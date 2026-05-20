c1253fb755666bf726ed1630d6daf6a497464ecb
Pritam Mondal <pritammondal18012003@gmail.com>
2026-05-21 00:46:25 +0530

    fix Admin seeder has hardcoded password

--- backend/config/createAdmin.js
+++ backend/config/createAdmin.js
@@ -1,17 +1,72 @@
+// const bcrypt = require('bcryptjs');
+// const User = require("../models/User");
+
+// const createAdmin = async () => {
+//   try {
+//     const adminExists = await User.findOne({
+//       email: process.env.ADMIN_EMAIL,
+//     });
+//     const hashedPassword = await bcrypt.hash(
+//     process.env.ADMIN_PASSWORD,
+//     10
+// );
+//     if (!adminExists) {
+//       await User.create({
+//         fullName: process.env.ADMIN_NAME,
+//         email: process.env.ADMIN_EMAIL,
+//         phone: "+91-9876543210",
+//         password: hashedPassword,
+//         role: "admin",
+//         isApproved: true,
+//         status: "approved",
+//         approvalDate: new Date(),
+//       });
+
+//       console.log("✅ Admin User Created Successfully");
+//     } else {
+//       console.log("✅ Admin already exists");
+//     }
+//   } catch (error) {
+//     console.error("❌ Error creating admin:", error.message);
+//   }
+// };
+
+// module.exports = createAdmin;
+
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 const createAdmin = async () => {
   try {
+
+    // Validate environment variables
+    if (
+      !process.env.ADMIN_NAME ||
+      !process.env.ADMIN_EMAIL ||
+      !process.env.ADMIN_PASSWORD
+    ) {
+      throw new Error("Admin environment variables are missing");
+    }
+
+    // Check if admin already exists
     const adminExists = await User.findOne({
-      email: "pritam.mondal@devqor.in",
+      email: process.env.ADMIN_EMAIL,
     });
 
     if (!adminExists) {
+
+      // Hash password
+      const hashedPassword = await bcrypt.hash(
+        process.env.ADMIN_PASSWORD,
+        10
+      );
+
+      // Create admin
       await User.create({
-        fullName: "Pritam Mondal",
-        email: "pritam.mondal@devqor.in",
+        fullName: process.env.ADMIN_NAME,
+        email: process.env.ADMIN_EMAIL,
         phone: "+91-9876543210",
-        password: "123456789",
+        password: hashedPassword,
         role: "admin",
         isApproved: true,
         status: "approved",
@@ -19,11 +74,11 @@ const createAdmin = async () => {
       });
 
       console.log("✅ Admin User Created Successfully");
-      console.log("📧 Email: pritam.mondal@devqor.in");
-      console.log("🔐 Password: 123456789");
+
     } else {
       console.log("✅ Admin already exists");
     }
+
   } catch (error) {
     console.error("❌ Error creating admin:", error.message);
   }
